9363bec80f5db25c1fd5bb52044f312eb08302e1
Dao Nguyen Huy Hoang <hoang.dnh210380@sis.hust.edu.vn>
2023-10-28 15:12:14 +0700

    done

--- Aims/src/TestPassingParameter.java
+++ Aims/src/TestPassingParameter.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class TestPassingParameter {
+public class TestPassingParameter {
 }
--- AimsProject2/src/tesst.java
+++ AimsProject2/src/tesst.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class tesst {
+public class tesst {
+    private String a;
+
+    public String getA() {
+        return a;
+    }
+
+    public tesst(String a, String b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public tesst(String b) {
+        this.b = b;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public void setB(String b) {
+        this.b = b;
+    }
+
+    private String b;
+
 }
