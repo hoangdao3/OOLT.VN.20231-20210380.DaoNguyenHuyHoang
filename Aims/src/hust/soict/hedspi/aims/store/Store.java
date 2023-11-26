cbc63d2e9f6e0fcc133074cb395c91d8786dd7f3
hoangdao3 <hoang.dnh210380@sis.hust.edu.vn>
2023-11-26 22:37:00 +0700

    section 12

--- Aims/src/hust/soict/hedspi/aims/store/Store.java
+++ Aims/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,42 +1,25 @@
 package Aims.src.hust.soict.hedspi.aims.store;
 
 import Aims.src.hust.soict.hedspi.aims.media.DigitalVideoDisc;
+import Aims.src.hust.soict.hedspi.aims.media.Media;
+
+
+import java.util.ArrayList;
 
 public class Store {
-    private DigitalVideoDisc[] itemsInStore;
-    private int totalItems;
+    private ArrayList<Media> itemsInStore = new ArrayList<>();
 
-    public Store(int maxSize) {
-        itemsInStore = new DigitalVideoDisc[maxSize];
-        totalItems = 0;
+    public void addMedia(Media media) {
+        itemsInStore.add(media);
+        System.out.println("Media added to the store.");
     }
 
-    public void addDVD(DigitalVideoDisc dvd) {
-        if (totalItems < itemsInStore.length) {
-            itemsInStore[totalItems] = dvd;
-            totalItems++;
-            System.out.println("DVD added to the store.");
+    public void removeMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            itemsInStore.remove(media);
+            System.out.println("Media removed from the store.");
         } else {
-            System.out.println("The store is full. Cannot add more DVDs.");
-        }
-    }
-
-    public void removeDVD(DigitalVideoDisc dvd) {
-        boolean found = false;
-        for (int i = 0; i < totalItems; i++) {
-            if (itemsInStore[i].equals(dvd)) {
-                found = true;
-                for (int j = i; j < totalItems - 1; j++) {
-                    itemsInStore[j] = itemsInStore[j + 1];
-                }
-                itemsInStore[totalItems - 1] = null;
-                totalItems--;
-                System.out.println("DVD removed from the store.");
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("DVD not found in the store.");
+            System.out.println("Media not found in the store.");
         }
     }
 }
